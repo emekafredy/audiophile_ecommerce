07a0149e498a0225ddf07063a81f2cc4fb725f55
emekafredy <emeka.s.chinedu@gmail.com>
2023-11-01 00:31:21 +0100

    add setup for testing components

---
+++ __tests__/components/NavBar.test.jsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import NavBar from '../../src/components/NavBar';
+
+describe('NavBar test', () => {
+  it('should contain the navbar menu', () => {
+    render(
+      <MemoryRouter>
+        <NavBar />
+      </MemoryRouter>,
+    );
+    const headphoneMenuOption = screen.getByText('Headphones');
+    const earphoneMenuOption = screen.getByText('Earphones');
+    const speakerMenuOption = screen.getByText('Speakers');
+
+    expect(headphoneMenuOption).toBeInTheDocument();
+    expect(earphoneMenuOption).toBeInTheDocument();
+    expect(speakerMenuOption).toBeInTheDocument();
+  });
+});
