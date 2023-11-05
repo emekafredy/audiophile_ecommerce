52d513148f618855ff7d6d6d1d21acb5584d7dda
emekafredy <emeka.s.chinedu@gmail.com>
2023-11-05 01:19:41 +0100

    scroll to top on re-route

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,45 @@
+import { useLayoutEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  useLocation,
+} from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 
 import Home from './pages/Home';
 import Category from './pages/Category';
 import Layout from './Layout';
 
+const Scroll = ({ children }) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children;
+};
+
 function App() {
   return (
     <BrowserRouter>
-      <div className="font-manrope">
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/:category" element={<Category />} />
-          </Route>
-        </Routes>
-        <ToastContainer />
-      </div>
+      <Scroll>
+        <div className="font-manrope">
+          <Routes>
+            <Route element={<Layout />}>
+              <Route
+                path="/"
+                element={<Home />}
+              />
+              <Route
+                path="/:category"
+                element={<Category />}
+              />
+            </Route>
+          </Routes>
+          <ToastContainer />
+        </div>
+      </Scroll>
     </BrowserRouter>
   );
 }
